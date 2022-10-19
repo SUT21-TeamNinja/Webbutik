e9ee159ca400b3e76ce20ef177b742d8c16f9921
Articunatu <62301779+Articunatu@users.noreply.github.com>
2022-10-19 13:25:21 +0200

    feat: User model with identity inheritance

--- Webbutik/Models/AppDbContext.cs
+++ Webbutik/Models/AppDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Webbutik.Models
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext:IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt){}
 
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
