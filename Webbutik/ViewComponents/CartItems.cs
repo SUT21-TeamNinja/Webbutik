78b55bedc241a33dd1036280230c1429416d5654
sigreyo <38696177+sigreyo@users.noreply.github.com>
2022-10-21 12:57:18 +0200

    checkpoint

---
+++ Webbutik/ViewComponents/CartItems.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Webbutik.Models;
+using Webbutik.ViewModels;
+
+namespace Webbutik.ViewComponents
+{
+    public class CartItems : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public CartItems(AppDbContext context) => _context = context;
+
+        //public async Task<IActionResult> InvokeAsync()
+        //{
+        //    return View(new CartViewModel
+        //    {
+        //        Cart = 
+        //    })
+        //}
+
+
+    }
+}
