bfd48cd69cf90a1ca07922f34307149f9ca6c928
sigreyo <38696177+sigreyo@users.noreply.github.com>
2022-10-21 14:26:36 +0200

    checkout methods

--- Webbutik/Controllers/OrderController.cs
+++ Webbutik/Controllers/OrderController.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Webbutik.Models;
 
 namespace Webbutik.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        private readonly Cart _cart;
+        public OrderController(AppDbContext context, Cart cart)
+        {
+            _context = context;
+            _cart = cart;
+        }
+        public async Task<IActionResult> CheckOutAsync()
+        {
+            _cart.CartItems = await _cart.GetCartItemsAsync();
+
+            if (_cart.CartItems.Count == 0)
+            {
+                return View("Empty");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> CheckOutAsync(Order order)
         {
             return View();
         }
