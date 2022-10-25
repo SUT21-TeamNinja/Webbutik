b36afc6f7bff46dd165b23f11e44324cabee46a7
elineri <elin.ericstam@gmail.com>
2022-10-25 10:58:15 +0200

    ManageCampaigns, NewCampaign, AddCampaign. Edited routes for Edit, Delete and Details

---
+++ Webbutik/ViewModels/DiscountViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Webbutik.Models;
+
+namespace Webbutik.ViewModels
+{
+    public class DiscountViewModel
+    {
+        public IEnumerable<Movie> MoviesOnSale { get; set; }
+        public IEnumerable<Movie> AllMovies { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
