4cb7e06ec58d2be09d9fef79878f43bf62fb0066
TheoAndren <theo.andren@gmail.com>
2022-10-25 14:52:02 +0200

    Small fix

--- Webbutik/Controllers/MoviesController.cs
+++ Webbutik/Controllers/MoviesController.cs
@@ -12,41 +12,11 @@ namespace Webbutik.Controllers
     public class MoviesController : Controller
     {
         private readonly AppDbContext _context;
-        private static bool IsUpdated = false;
 
         public MoviesController(AppDbContext context)
         {
             _context = context;
         }
-
-        public async Task<IActionResult> Start()
-        {
-            var list = await FakeMovies.GetMoviesFromApi();
-            if (!IsUpdated)
-            {
-                await Populate(list);
-                IsUpdated = true;
-            }
-            return View();
-        }
-        
-        public async Task Populate(List<Movie> list)
-        {
-            for (int i = 1; i < _context.Movies.Count(); i++)
-            {
-                var movie = await _context.Movies.FirstOrDefaultAsync(x => x.Id == i);
-                movie.Title = list[i].Title;
-                movie.Description = list[i].Description;
-                movie.ImageUrl = list[i].ImageUrl;
-                movie.ReleaseDate = list[i].ReleaseDate;
-                movie.Stars = list[i].Stars;
-                movie.Writers = list[i].Writers;
-                movie.Directors = list[i].Directors;
-                Console.WriteLine("updated");
-            }
-            await _context.SaveChangesAsync();
-        }
-
         // GET: Movies
         public async Task<IActionResult> Index()
         {
