80fc5d4747ce0a9bbd660ea6636be33f1bdf28e3
Daniel Thulin <daniel_thulin@outlook.com>
2022-10-27 11:42:48 +0200

    Missing a {

--- Webbutik/Controllers/OrderController.cs
+++ Webbutik/Controllers/OrderController.cs
@@ -8,6 +8,7 @@ using Webbutik.Models;
 namespace Webbutik.Controllers
 {
     public class OrderController : Controller
+    { 
 
         private readonly AppDbContext _context;
         private readonly Cart _cart;
