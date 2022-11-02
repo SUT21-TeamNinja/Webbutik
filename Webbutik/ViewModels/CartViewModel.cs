2d74e2c0becf90ec81a6d74fa07b5ada810c5b24
sigreyo <38696177+sigreyo@users.noreply.github.com>
2022-11-02 13:37:03 +0100

    total price added

--- Webbutik/ViewModels/CartViewModel.cs
+++ Webbutik/ViewModels/CartViewModel.cs
@@ -5,6 +5,7 @@ namespace Webbutik.ViewModels
     public class CartViewModel
     {
         public Cart Cart { get; set; }
-        public int Total { get; set; }
+        public int TotalItems { get; set; }
+        public int? CartTotal { get; set; }
     }
 }
